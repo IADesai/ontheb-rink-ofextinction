0e3c4d8e4c063efef7ddc5a3de08aaf6bff66572
Ibrahim Ayub Desai <113534999+IADesai@users.noreply.github.com>
2023-08-29 15:20:51 +0100

    send_email

---
+++ live_pipeline/load/load.py
@@ -0,0 +1,36 @@
+from os import environ
+from boto3 import client
+from botocore.client import BaseClient
+from dotenv import load_dotenv
+
+def send_email(config, task: str, message: str):
+    """Function that sends out emails"""
+    email = client('ses', aws_access_key_id=config["ACCESS_KEY_ID"],
+                   aws_secret_access_key=config["SECRET_ACCESS_KEY"])
+    response = email.send_email(
+        Source=config["EMAIL"],
+        Destination={
+            'ToAddresses': [
+                config["EMAIL"],
+            ]
+        },
+        Message={
+            'Subject': {
+                'Data': f'{task}',
+            },
+            'Body': {
+                'Html': {
+                    'Data': f'{message}',
+                }
+            }
+        }
+    )
+
+if __name__ == "__main__":
+
+    load_dotenv()
+    config = environ
+
+    example_message = "This is is a test message to test that the email is being sent as per expected"
+
+    send_email(config, "example task", example_message)
