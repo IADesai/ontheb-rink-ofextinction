33b00b674e6211fb548f773f0373d253b7e18313
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-08-29 15:44:49 +0100

    schema made

--- warehouse_pipeline/schema.sql
+++ warehouse_pipeline/schema.sql
@@ -2,29 +2,52 @@ CREATE DATABASE plants;
 \c plants
 
 
-CREATE TABLE IF NOT EXISTS stories (
-    id SERIAL PRIMARY KEY,
-    title text NOT NULL UNIQUE,
-    url text NOT NULL UNIQUE,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL
+CREATE TABLE IF NOT EXISTS species (
+    species_name_id SERIAL PRIMARY KEY,
+    scientific_name text NOT NULL UNIQUE,
+    species_id SMALLINT NOT NULL UNIQUE
 );
 
 
-INSERT INTO stories(title, url, created_at, updated_at) VALUES ('Actor released from prison in sex-trafficking case', 'https://www.bbc.co.uk/news/world-us-canada-66111026', '2023-07-06 13:38:33.238446', '2023-07-06 13:38:33.238446');
-INSERT INTO stories(title, url, created_at, updated_at) VALUES ('Adele speaks out against audiences throwing objects on stage', 'https://www.bbc.co.uk/news/newsbeat-66108108', '2023-07-06 13:38:33.238922', '2023-07-06 13:38:33.238922');
-INSERT INTO stories(title, url, created_at, updated_at) VALUES ('"I want more people to feel like this is a sport for them"', 'https://www.bbc.co.uk/sport/diving/66093123', '2023-07-06 13:38:33.240815', '2023-07-06 13:38:33.240815');
+CREATE TABLE IF NOT EXISTS sunlight (
+    sunlight_id SERIAL PRIMARY KEY,
+    s_description text NOT NULL
+);
 
 
-CREATE TABLE votes (
-    id SERIAL PRIMARY KEY,
-    direction text DEFAULT 'up'::text NOT NULL,
-    created_at timestamp without time zone NOT NULL,
-    updated_at timestamp without time zone NOT NULL,
-    story_id INT,
-    CONSTRAINT fk_story_id
-        FOREIGN KEY(story_id)
-            REFERENCES stories(id)
+CREATE TABLE IF NOT EXISTS cycle (
+    cycle_id SERIAL PRIMARY KEY,
+    cycle_name text
+);
+
+
+CREATE TABLE IF NOT EXISTS botanist (
+    botanist_id SERIAL PRIMARY KEY,
+    b_name text NOT NULL,
+    b_email text NOT NULL,
+    b_phone SMALLINT NOT NULL
+);
+
+
+CREATE TABLE plant (
+    species_id SERIAL PRIMARY KEY,
+    temperature FLOAT,
+    soil_moisture FLOAT,
+    humidity FLOAT,
+    last_watered TIMESTAMP,
+    recording_taken TIMESTAMP NOT NULL,
+    sunlight_id SMALLINT,
+    botanist_id SMALLINT, 
+    cycle_id SMALLINT,
+    CONSTRAINT fk_sunlight_id
+        FOREIGN KEY(sunlight_id)
+            REFERENCES sunlight(sunlight_id),
+    CONSTRAINT fk_botanist_id
+        FOREIGN KEY(botanist_id)
+            REFERENCES botanist(botanist_id),
+    CONSTRAINT fk_cycle_id
+        FOREIGN KEY(cycle_id)
+            REFERENCES cycle(cycle_id)
 );
 
 
