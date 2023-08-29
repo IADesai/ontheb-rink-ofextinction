c56084f1a2c042fa6f60188bf10f40945fd00ef8
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-08-29 21:56:59 +0100

    all

--- warehouse_pipeline/load_to_database.py
+++
@@ -1,7 +0,0 @@
-from dotenv import dotenv_values
-
-from database_functions import get_db_connection
-
-if __name__ == '__main__':
-    configuration = dotenv_values()
-    connection = get_db_connection(configuration)
--- warehouse_pipeline/test_database_functions.py
+++
@@ -1,7 +0,0 @@
-'''libraries required to test database functions'''
-import pytest
-from database functions import add_cycle_information, add_botanist_information, add_species_information, add_plant_information
-
-
-"""this remains to be updated as I will likely need to change the format of these in my transform function and this may change 
-the actual function """
