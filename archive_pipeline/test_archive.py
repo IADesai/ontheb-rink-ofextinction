7c8c075295cd4410a3fa55505f7c9e39a5d886a2
rk145lr <132599493+rk145lr@users.noreply.github.com>
2023-08-30 18:08:10 +0000

    #7 Created unit tests for the creation of a DataFrame for holding the deleted rows. Added .coverage to .gitignore.

--- archive_pipeline/test_archive.py
+++ archive_pipeline/test_archive.py
@@ -1,8 +1,12 @@
 """Contains unit tests to be run with pytest for the functions relating to the archiving of data older than 24 hours."""
 
 from re import match
+from unittest.mock import patch
 
-from archive import get_previous_day_timestamp
+import pytest
+import pandas as pd
+
+from archive import get_previous_day_timestamp, create_deleted_rows_dataframe
 
 
 def test_timestamp_returns_string():
@@ -21,3 +25,18 @@ def test_timestamp_formatted_correctly():
     """Tests the timestamp is formatted in a way recognised by PostgreSQL."""
     res = get_previous_day_timestamp()
     assert match(r"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}", res)
+
+
+@patch("archive.CSV_COLUMNS", ["column_a", "column_b"])
+def test_data_frame_returned_from_creation():
+    """Tests a DataFrame is returned if valid data is supplied."""
+    deleted_rows = [(1, 2), (3, 4)]
+    res = create_deleted_rows_dataframe(deleted_rows)
+    assert isinstance(res, pd.DataFrame)
+
+
+def test_exception_raised_empty_dataframe():
+    """Checks an exception is raised if there are no deleted rows to create a DataFrame."""
+    deleted_rows = []
+    with pytest.raises(ValueError):
+        create_deleted_rows_dataframe(deleted_rows)
