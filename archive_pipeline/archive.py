9bcd6440be5c4036a2cf7f2feebfb8a3065a85a0
rk145lr <132599493+rk145lr@users.noreply.github.com>
2023-08-31 09:33:56 +0100

    #7 Added SELECT statement before DELETE to maintain consistency with data formatting of the dashboard.

--- archive_pipeline/archive.py
+++ archive_pipeline/archive.py
@@ -11,12 +11,12 @@ from psycopg2 import connect
 import pandas as pd
 from boto3 import client
 
-CSV_COLUMNS = ["plant_entry_id", "species_id", "temperature", "soil_moisture",
-                "humidity", "last_watered", "recording_taken", "sunlight_id",
-                "botanist_id", "cycle_id"]
+CSV_COLUMNS = ["entry_id", "species", "temperature", "soil_moisture",
+               "humidity", "last_watered", "recording_taken", "sunlight",
+               "botanist_name", "cycle"]
 
 
-def get_database_connection(config: dict): # pragma: no cover
+def get_database_connection(config: dict):  # pragma: no cover
     """Establishes a connection with the PostgreSQL database."""
     try:
         return connect(
@@ -38,12 +38,13 @@ def get_previous_day_timestamp() -> str:
     return previous_day_str
 
 
-def delete_old_rows(conn, delete_timestamp: str) -> list[tuple]: # pragma: no cover
+def delete_old_rows(conn, delete_timestamp: str) -> list[tuple]:  # pragma: no cover
     """Removes rows from the Plant table that are older than a day.
 
     Returns the deleted rows.
     """
     with conn.cursor() as cur:
+        print("Deleting rows from RDS.")
         cur.execute(
             """DELETE FROM plant
             WHERE recording_taken < %s
@@ -51,7 +52,51 @@ def delete_old_rows(conn, delete_timestamp: str) -> list[tuple]: # pragma: no co
         deleted_rows = cur.fetchall()
         conn.commit()
         cur.close()
+        print("Rows deleted from RDS.")
         return deleted_rows
+
+
+def get_deleted_rows(conn, delete_timestamp: str) -> list[tuple]:  # pragma: no cover
+    """Returns the rows that were recorded more than a day ago."""
+    with conn.cursor() as cur:
+        print("Fetching data to archive from RDS.")
+        cur.execute("""
+            SELECT p.plant_entry_id AS entry_id,
+                s.scientific_name AS species,
+                p.temperature,
+                p.soil_moisture,
+                p.humidity,
+                p.last_watered AS last_watered,
+                p.recording_taken AS recording_taken,
+                sun.s_description AS sunlight,
+                b.b_name AS botanist_name,
+                c.cycle_name AS cycle
+            FROM plant p
+            LEFT JOIN species s ON p.species_id = s.species_id
+            LEFT JOIN sunlight sun ON p.sunlight_id = sun.sunlight_id
+            LEFT JOIN botanist b ON p.botanist_id = b.botanist_id
+            LEFT JOIN cycle c ON p.cycle_id = c.cycle_id
+            WHERE recording_taken < %s;""", (delete_timestamp,))
+        deleted_rows = cur.fetchall()
+        conn.commit()
+        cur.close()
+        print("Archive data fetched from RDS.")
+        return deleted_rows
+
+
+def select_and_delete_from_db(conn, delete_timestamp: str) -> list[tuple]:  # pragma: no cover
+    """Removes rows from the plant table that are older than a day.
+
+    SELECT is performed to maintain consistency with live databatase interactions in the dashboard.
+    Returns the deleted rows.
+    """
+    rows_to_delete = get_deleted_rows(conn, delete_timestamp)
+    deleted_rows = delete_old_rows(conn, delete_timestamp)
+
+    if len(rows_to_delete) != len(deleted_rows):
+        print("Inconsistency between rows being deleted and rows being archived. " +
+              "Script will continue.")
+    return rows_to_delete
 
 
 def create_deleted_rows_dataframe(deleted_rows: list[tuple]) -> pd.DataFrame:
@@ -78,23 +123,24 @@ def create_archived_csv_file(archived_df: pd.DataFrame, csv_filename: str) -> No
     archived_df.to_csv(csv_filename, index=False)
 
 
-def upload_csv_to_s3(csv_filename: str, config: dict) -> None: # pragma: no cover
+def upload_csv_to_s3(csv_filename: str, config: dict) -> None:  # pragma: no cover
     """Uploads the created .csv file to an S3 bucket."""
     print("Establishing connection to AWS.")
     s3_client = client("s3", aws_access_key_id=config["ACCESS_KEY_ID"],
                        aws_secret_access_key=config["SECRET_ACCESS_KEY"])
     print("Connection established.")
     print("Uploading .csv file.")
-    s3_client.upload_file(csv_filename, config["ARCHIVE_BUCKET_NAME"], csv_filename)
+    s3_client.upload_file(
+        csv_filename, config["ARCHIVE_BUCKET_NAME"], csv_filename)
     print(".csv file uploaded.")
 
 
-if __name__ == "__main__": # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     configuration = dotenv_values()
     connection = get_database_connection(configuration)
 
     timestamp = get_previous_day_timestamp()
-    list_deleted_rows = delete_old_rows(connection, timestamp)
+    list_deleted_rows = select_and_delete_from_db(connection, timestamp)
 
     deleted_rows_df = create_deleted_rows_dataframe(list_deleted_rows)
     archived_csv_filename = create_csv_filename()
